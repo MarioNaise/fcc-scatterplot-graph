902c6ba29360316deb16fce2100c391d04ebdb18
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-12-22 17:16:09 +0000

    implement yAxis (WIP)

--- script.js
+++ script.js
@@ -12,30 +12,44 @@
     console.log(dataset)
     const w = 1000;
     const h = 700;
-    const padding = 50;
+    const padding = 100;
 
     const svg = d3.select("main")
       .append("svg")
       .attr("width", w)
       .attr("height", h)
-  
-    console.log()
 
     const yearsDate = dataset.map((item)=>{
-      return new Date(item.Year+"-01-01");
+      return new Date(item.Year + "-01-01");
     });
-    console.log([d3.min(yearsDate), d3.max(yearsDate)])
+    
+    const timeData = dataset.map((item)=>{
+      let timeArr = item.Time.split(":");
+      return parseInt(timeArr[0]) + parseFloat((1 / 60 ) * timeArr[1]);
+    });
+
+    console.log(d3.max(timeData))
 
     const xScale = d3.scaleTime()
       .domain([d3.min(yearsDate), d3.max(yearsDate)])
       .range([padding, w - padding]);
 
+    const yScale = d3.scaleLinear()
+      .domain([d3.max(timeData), d3.min(timeData)])
+      .range([h - padding,padding]);
+
     const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
     
     svg.append("g")
        .attr("transform", "translate(0," + (h - padding) + ")")
        .call(xAxis)
        .attr("id", "x-axis");
+
+    svg.append("g")
+       .attr("transform", "translate(" + padding + ",0)")
+       .call(yAxis)
+       .attr("id", "y-axis");
     
   }
 
