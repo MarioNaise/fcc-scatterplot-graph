25cff67c7f6451825ead9ef776411b84246a4fb4
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-12-22 10:50:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+(function() {
+  const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+
+  const getData = async () => {
+    const response = await fetch(dataUrl);
+    const dataObj = await response.json();
+    return await dataObj;
+  }
+
+  const renderData = async () => {
+    const dataset = await getData();
+    console.log(dataset)
+    const w = 1000;
+    const h = 700;
+    const padding = 50;
+  }
+
+  renderData();
+})()
